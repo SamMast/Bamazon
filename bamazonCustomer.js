b5069c54a0a7a3ba637c557ac242191808794aeb
Samuel Mast <samuel.a.mast@gmail.com>
2018-02-21 19:52:04 -0700

    almost complete.  Bugs with the call back function for run and callback for again

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,9 +12,134 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
+  	if (err) throw err;
 
-  console.log("connected as id " + connection.threadId);
-  
+  	console.log("connected as id " + connection.threadId);
+  	
+  	// displayItems(run());
+	run();
+
 });
+
+
+function displayItems(cb) {
+  	var query = connection.query("SELECT * FROM products", function(err, res) {
+	    if (err) throw err;
+
+	    console.log(`\n.......................\nCurrent Items For Sale:\n------------`)
+
+	    for (var i = 0; i < res.length; i++) {
+	      console.log(`Item ID: ${res[i].item_id}\nItem Name: ${res[i].product_name}\nPrice: $${res[i].price}\n------------`);
+	    }
+
+	    console.log(".......................")
+
+	    cb();
+
+  	});
+
+}
+
+
+function run() {
+
+	inquirer.prompt([
+	  {
+	    name: "ID",
+	    message: "What is the ID of the item you would like to purchase?",
+	    type: "input"
+        // validate: function(value) {
+        //   if (value > 0) {
+        //     return true;
+        //   }
+        //   return false;
+        // }
+	  },
+	  {
+	    name: "amount",
+	    message: "How many would you like to buy?",
+	    type: "input"
+	  }
+	]).then(function(answers) {
+
+		var query = connection.query("SELECT * FROM products WHERE ?", {item_id: answers.ID}, function(err, res) {
+	    	if (err) throw err;
+
+	    	var amount = parseInt(answers.amount);
+	    	var quantity = parseInt(res[0].stock_quantity)
+
+	    	if (amount > quantity) {
+
+	    		console.log("Insufficient Stock Left")
+	    		again();
+
+	    	} else {
+	    		var leftover = (quantity - amount);
+			// Update the SQL database to reflect the remaining quantity.
+				updateItem(answers.ID, leftover);
+
+			// Once the update goes through, show the customer the total cost of their purchase.
+				showCost(amount, answers.ID);
+
+				again();
+	    	}
+
+		});		
+
+
+
+	});
+}
+
+
+function updateItem(ID, leftover) {
+	var obj = {};
+	var objB = {};
+
+	obj.stock_quantity = leftover;
+	objB.item_id = ID;
+
+  	var query = connection.query(
+    	"UPDATE products SET ? WHERE ?",
+	    [obj, objB],
+	    function(err, response) {
+	      	console.log("Product(s) bought!\n");
+	    }
+  	);
+
+}
+
+function showCost(amountPurchased, ID) {
+  	var query = connection.query("SELECT * FROM products WHERE ?", {item_id: ID}, function(err, res) {
+		if (err) throw err;
+		var cost = res[0].price * amountPurchased; 
+		console.log(`Total cost for ${amountPurchased} ${res[0].product_name}(s) is: $${cost}`)
+
+	});
+
+}
+
+function again() {
+	inquirer.prompt([
+	    {
+	      type: "confirm",
+	      message: "Back to the Store?",
+	      name: "confirm",
+	      default: true
+	    }
+	]).then(function(answer) {
+
+		if (answer.confirm) {
+			run();
+		} else {
+	    	connection.end();
+		}
+    
+  });
+
+}
+
+
+
+
 
